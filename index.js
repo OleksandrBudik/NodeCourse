b4bd45351546f36f7ac274fa005f91dfc78e610f
Oleksandr Budik <obudik@lohika.com>
2020-09-25 11:35:02 +0300

    mint fix

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const { config } = require('./modules/config');
 const { Logger } = require('./modules/logger');
 const http = require('http');
 
+const port = config.PORT
+
 http.createServer((req, res) => {
   Logger.log('Hello world');
   res.writeHeader(200, { 'Content-type': 'application/json' });
   res.end(JSON.stringify({ message: 'Hello world! '}));
-}).listen(config.PORT, () => Logger.warn(`Listening on port ${config.PORT}...`));
+}).listen(port, () => Logger.warn(`Listening on port ${port}...`));
 
 Logger.debug('App args are', config.ARGS);
 Logger.debug('App envs are', config.ENVS);
